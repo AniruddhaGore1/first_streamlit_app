15878bf4fbd7141a74cde2fc3aa57d50b9e2b456
Aniruddha Gore <121605869+AniruddhaGore1@users.noreply.github.com>
2022-12-29 14:42:57 +0530

    added emojis
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("Happy New year!")
+st.title("🎉Happy New year!🎉")
 st.header("Welcome to year 2023")
 st.text("This is yet another year.")
-st.text("We thought, COVID is over. There are traces of it out there.")
-st.text("Be careful! Be Safe! Wear mask!")
+st.text("We thought, COVID is over.🦠 There are traces of it out there.")
+st.text("Be careful! Be Safe! 😷Wear mask!")
