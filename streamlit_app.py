9f0bb5dd3d4ee1ef3fee54e8d4e4e6742aaf8dc4
Aniruddha Gore <121605869+AniruddhaGore1@users.noreply.github.com>
2022-12-29 14:39:55 +0530

    Adding some lines to the st app
    just adding some lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title("Happy New year!")
+st.header("Welcome to year 2023")
+st.text("This is yet another year.")
+st.text("We thought, COVID is over. There are traces of it out there.")
+st.text("Be careful! Be Safe! Wear mask!")
